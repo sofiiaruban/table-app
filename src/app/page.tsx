7ed883ce617c8e357c668e28d4dfc93694fad71f
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-25 08:30:23 +0300

    bugfix: Change path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import Login from './components/login';
+import Login from './components/Login';
 import {useRouter} from 'next/navigation'
 
 export default function Auth() {

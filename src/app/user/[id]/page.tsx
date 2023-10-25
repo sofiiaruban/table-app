d8fe81cf14f4e4932cf4ef9db6184e4fe145282e
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-25 20:23:06 +0300

    bugfix: Debugging routing

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,9 +1,10 @@
-import UserForm from "@/app/components/UserForm";
+// import UserForm from "@/app/components/UserForm";
 
 export default function EditUser() {
   return (
     <div className="w-full flex flex-col justify-center items-center h-screen">
-      <UserForm editMode={true} />
+      EDIT USER FORM
+      {/* <UserForm editMode={true} /> */}
     </div>
   )
 }
